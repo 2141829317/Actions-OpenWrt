8cfe6172aaa234aaa4be8ddcbcd260a0cf36c903
2141829317 <112305664+2141829317@users.noreply.github.com>
2026-03-10 14:33:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git ua2f https://github.com/Zxilly/UA2F.git' >> feeds.conf.default
+echo 'src-git mentohust https://github.com/BoringCat/luci-app-mentohust.git' >> feeds.conf.default
